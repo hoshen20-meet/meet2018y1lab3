1e770a428e6cc0bdcc799cce5de171e7f98ff67a
hoshen20-meet <hoshen20@meet.mit.edu>
2018-07-31 07:25:33 -0400

    Stopping here

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+Language = "English"
+Translation = "Hello World"
+
+print('The translation of hello world in ' + Language + 'is' + Translation)
+Language = "Italian"
+
+Translation = "Ciao Mondo"
+
+      
